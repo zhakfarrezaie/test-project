36d6e354c108560c3ba1b3da48faa06a08292935
zhakfarrezaie <zhakfar01123@gmail.com>
2026-02-15 16:43:59 +0430

    practice variable

---
+++ script.js
@@ -0,0 +1,50 @@
+let thing = {
+    type : "book" ,
+    page : 100 ,
+    tittle : "human" ,
+}
+console.log(thing);
+
+let users = ["ali","ahmad"];
+users[2] = "reza";
+users[0] = "alice";
+console.log(users);
+
+let number = 10;
+number = "Ali";
+console.log(number);
+let name = "ahmad zhakfar";
+console.log(name.length);
+
+let a = (10 > 5);
+console.log(a);
+
+let b = ("ahmad" == "zhakfar");
+console.log(b);
+
+let c = 700;
+console.log(typeof c);
+
+let d = ("ali");
+console.log(typeof d);
+
+let e = true;
+console.log(typeof e);
+
+let f;
+console.log(typeof f);
+
+let g = '10' + '5';
+console.log(g);
+console.log(typeof g);
+
+let h = 2 + 3 * 4;
+console.log(h);
+console.log(typeof h);
+
+let i = ( 2 + 3 ) * 4;
+console.log(i);
+
+let j = (7 + 9) * 6;
+console.log(" b = " + j);
+console.log(typeof j);
